a3b4a9236033d13c92c7a3d21ff93c3abaf4c3dc
hunjoonrhee <hunjoonrhee90@gmail.com>
2022-10-28 09:15:19 +0200

    Issue #9, #2: Search Routes nearby input address

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,38 +1,13 @@
-import React, {ChangeEvent, FormEvent, useEffect, useState} from 'react';
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
-import axios from "axios";
 import {HashRouter, Route, Routes} from "react-router-dom";
 import MainPage from "./pages/MainPage";
 import RoutesPage from "./pages/RoutesPage";
-import {LocationRequest} from "./model/LocationRequest";
+import useMyRoutes from "./hooks/useMyRoutes";
 
 function App() {
 
-
-    const [locationRequest, setLocationRequest] = useState("");
-
-    function setRequest(locationRequest:string){
-        setLocationRequest(locationRequest);
-        console.log(locationRequest)
-    }
-
-    const [routes, setRoutes] = useState([]);
-
-    function getRoutesNearByLocationRequest(locationRequest:string){
-        if(locationRequest.length>0){
-            console.log("ddd", locationRequest)
-            axios.get(`/api/route/routes?address=${locationRequest}`)
-                .then((response)=> response.data)
-                .then((routes) => setRoutes(routes))
-                .catch((err)=>console.log((err)))
-        }
-    }
-
-    useEffect(()=>{
-        getRoutesNearByLocationRequest(locationRequest)
-    }, [])
-
+    const {setRequest, routes, getRoutesNearByLocationRequest} = useMyRoutes()
 
     return (
     <div className="App">
@@ -42,7 +17,9 @@ function App() {
             <Routes>
                 <Route path = {"/"} element = {<MainPage setRequest={setRequest}
                                                          getRoutesNearByLocationRequest={getRoutesNearByLocationRequest}/>}/>
-                <Route path = {"/routes"} element = {<RoutesPage routes={routes}/>}/>
+                <Route path = {"/routes"} element = {<RoutesPage routes={routes}
+                                                                 setRequest={setRequest}
+                                                                 getRoutesNearByLocationRequest={getRoutesNearByLocationRequest}/>}/>
             </Routes>
         </HashRouter>
 
--- frontend/src/pages/MainPage.tsx
+++ frontend/src/pages/MainPage.tsx
@@ -1,4 +1,4 @@
-import React, {ChangeEvent, FormEvent, useEffect, useState} from "react";
+import React, {ChangeEvent, useState} from "react";
 import {Link} from "react-router-dom";
 import "./MainPage.css"
 
@@ -8,51 +8,36 @@ type MainPageProps={
 }
 
 export default function MainPage(props:MainPageProps){
-    console.log("mainpage rendered")
-
-
     const [location, setLocation] = useState("");
     function handleChange(event:ChangeEvent<HTMLInputElement>) {
         const inputFieldValue = event.target.value;
         setLocation(inputFieldValue);
-        console.log(location)
-
     }
 
     const handleLinkClick = () =>{
-        // event.preventDefault();
         console.log("button is clicked!")
         props.setRequest(location)
-        console.log(location)
         props.getRoutesNearByLocationRequest(location);
-        // setLocation("");
     }
 
     return(
         <div className={"mainpage"}>
             <section className={"sec-title"}>
-                blablabla
+                <h2 id={"title"}>Running Route Advisor</h2>
             </section>
             <section className={"sec-search"}>
-                <form className={"form-searchField"}>
-
-                    <label>
-                        <input type="text" className="form-control" placeholder="Where do you want to run?" name = "location"
-                        aria-label="Recipient's username" aria-describedby="button-addon2" value={location}
-                        onChange={handleChange}/>
-                    </label>
-                    <Link onClick={handleLinkClick} to={"/routes"}>
-                        <button className="btn btn-outline-secondary" type="submit" id="button-addon2">Search</button>
-                    </Link>
-
-                    {/*<label>*/}
-                    {/*    <input className={"form-input"} name={"location"} type={"text"} value={location}*/}
-                    {/*           placeholder={"Where do you want to run?"} onChange={handleChange}/>*/}
-                    {/*</label>*/}
-                    {/*<Link onClick={handleLinkClick} to={"/routes"}>*/}
-                    {/*    <button className={"form-submit"} type={"submit"}>Search</button>*/}
-                    {/*</Link>*/}
-                </form>
+                <div className={"form-searchField"}>
+                    <form>
+                        <label className={"form-input"}>
+                            <input type="text" className="form-control" placeholder="Where do you want to run?" name = "location"
+                                   aria-label="Recipient's username" aria-describedby="button-addon2" value={location}
+                                   onChange={handleChange}/>
+                        </label>
+                        <Link onClick={handleLinkClick} to={"/routes"}>
+                            <button className="btn btn-outline-secondary" type="submit" id="button-addon2">Search</button>
+                        </Link>
+                    </form>
+                </div>
             </section>
         </div>
     )
