5793bb86fd393778c247efe1e3872c51b7a47de4
hunjoonrhee <hunjoonrhee90@gmail.com>
2022-11-11 16:59:01 +0100

    Fix Test FoundRoute

--- backend/src/test/java/de/neuefische/backend/controller/FoundRouteControllerTest.java
+++ backend/src/test/java/de/neuefische/backend/controller/FoundRouteControllerTest.java
@@ -1,9 +1,11 @@
 package de.neuefische.backend.controller;
 
+import de.neuefische.backend.model.EndPosition;
 import de.neuefische.backend.model.FoundRoutes;
 import de.neuefische.backend.model.Route;
 import de.neuefische.backend.model.StartPosition;
 import de.neuefische.backend.repository.FoundRouteRepository;
+import de.neuefische.backend.service.FoundRouteService;
 import de.neuefische.backend.service.RouteService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,6 +19,7 @@ import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.mockito.ArgumentMatchers.any;
@@ -37,6 +40,8 @@ class FoundRouteControllerTest {
     private FoundRouteRepository foundRouteRepository;
     @MockBean
     private RouteService routeService;
+    @MockBean
+    private FoundRouteService foundRouteService;
 
 
     @Test
@@ -47,48 +52,69 @@ class FoundRouteControllerTest {
         String[] hashtags = new String[1];
         hashtags[0] = "tree";
         StartPosition startPosition = new StartPosition(2.2, 1.1);
+        EndPosition endPosition = new EndPosition(2.3, 1.12);
         List<Route> fakeFoundRoutes = List.of(
                 Route.builder()
                         .id("1")
                         .routeName("test")
                         .hashtags(hashtags)
-                        .imageThumbnail("imageThumbnail")
+                        .imageThumbnail("test1")
                         .startPosition(startPosition)
+                        .betweenPositions(new ArrayList<>())
+                        .endPosition(endPosition)
+                        .routes(null)
+                        .photos(new ArrayList<>())
                         .position(new GeoJsonPoint(2.2, 1.1))
+                        .createdBy("user1")
                         .build()
         );
 
         when(routeService.findByRoutesNear(any())).thenReturn(
                 fakeFoundRoutes
         );
+        FoundRoutes foundRoutes = new FoundRoutes("test", fakeFoundRoutes);
+        when(foundRouteService.saveFoundRoutes("test")).thenReturn(foundRoutes);
 
         String expectedJSON = """
-                    {
-                         "id": "test",
-                         "routes": [
-                             {
-                                 "id": "1",
-                                 "routeName": "test",
-                                 "hashtags": [
-                                     "tree"
-                                 ],
-                                 "imageThumbnail": "imageThumbnail",
-                                 "startPosition": {
-                                     "lat": 2.2,
-                                     "lon": 1.1
-                                 },
-                                 "position": {
-                                     "x": 2.2,
-                                     "y": 1.1,
-                                     "type": "Point",
-                                     "coordinates": [
-                                         2.2,
-                                         1.1
-                                     ]
-                                 }
-                             }
-                         ]
-                     }
+                            {
+                                "id": "test",
+                                "routes": [
+                                    {
+                                        "id": "1",
+                                        "routeName": "test",
+                                        "hashtags": [
+                                            "tree"
+                                        ],
+                                        "imageThumbnail": "test1",
+                                        "startPosition": {
+                                            "lat": 2.2,
+                                            "lon": 1.1
+                                        },
+                                        "betweenPositions":[
+                                        ],
+                                        "endPosition": {
+                                            "lat": 2.3,
+                                            "lon": 1.12
+                                        },
+                                        "routes": null,
+                                        "photos":[
+                                        ],
+                                        "position": {
+                                            "x": 2.2,
+                                            "y": 1.1,
+                                            "type": "Point",
+                                            "coordinates": [
+                                                2.2,
+                                                1.1
+                                            ]
+                                        },
+                                        "createdBy":"user1"
+                                    }
+                                
+                                ]
+                            
+                            }
+
                 """;
         // WHEN & THEN
 
@@ -107,45 +133,62 @@ class FoundRouteControllerTest {
         String[] hashtags = new String[1];
         hashtags[0] = "tree";
         StartPosition startPosition = new StartPosition(2.2, 1.1);
-        FoundRoutes foundRoutes = new FoundRoutes("testaddress", List.of(
+        EndPosition endPosition = new EndPosition(2.3, 1.12);
+        FoundRoutes foundRoutes = new FoundRoutes("test", List.of(
                 Route.builder()
                         .id("1")
                         .routeName("test")
                         .hashtags(hashtags)
-                        .imageThumbnail("imageThumbnail")
+                        .imageThumbnail("test1")
                         .startPosition(startPosition)
+                        .betweenPositions(new ArrayList<>())
+                        .endPosition(endPosition)
+                        .routes(null)
+                        .photos(new ArrayList<>())
                         .position(new GeoJsonPoint(2.2, 1.1))
+                        .createdBy("user1")
                         .build()
         ));
-        foundRouteRepository.save(foundRoutes);
+        when(foundRouteService.getAllFoundRoutes()).thenReturn(List.of(foundRoutes));
 
         String expectedJSON = """
                     [
                         {
-                         "id": "testaddress",
+                         "id": "test",
                          "routes": [
-                             {
-                                 "id": "1",
-                                 "routeName": "test",
-                                 "hashtags": [
-                                     "tree"
-                                 ],
-                                 "imageThumbnail": "imageThumbnail",
-                                 "startPosition": {
-                                     "lat": 2.2,
-                                     "lon": 1.1
-                                 },
-                                 "position": {
-                                     "x": 2.2,
-                                     "y": 1.1,
-                                     "type": "Point",
-                                     "coordinates": [
-                                         2.2,
-                                         1.1
-                                     ]
-                                 }
-                             }
-                         ]
+                                    {
+                                        "id": "1",
+                                        "routeName": "test",
+                                        "hashtags": [
+                                            "tree"
+                                        ],
+                                        "imageThumbnail": "test1",
+                                        "startPosition": {
+                                            "lat": 2.2,
+                                            "lon": 1.1
+                                        },
+                                        "betweenPositions":[
+                                        ],
+                                        "endPosition": {
+                                            "lat": 2.3,
+                                            "lon": 1.12
+                                        },
+                                        "routes": null,
+                                        "photos":[
+                                        ],
+                                        "position": {
+                                            "x": 2.2,
+                                            "y": 1.1,
+                                            "type": "Point",
+                                            "coordinates": [
+                                                2.2,
+                                                1.1
+                                            ]
+                                        },
+                                        "createdBy":"user1"
+                                    }
+                                
+                                ]
                      }
                     ]
                     
@@ -165,46 +208,65 @@ class FoundRouteControllerTest {
         String[] hashtags = new String[1];
         hashtags[0] = "tree";
         StartPosition startPosition = new StartPosition(2.2, 1.1);
-        FoundRoutes foundRoutes = new FoundRoutes("testaddress", List.of(
+        EndPosition endPosition = new EndPosition(2.3, 1.12);
+        List<Route> fakeFoundRoutes = List.of(
                 Route.builder()
                         .id("1")
                         .routeName("test")
                         .hashtags(hashtags)
-                        .imageThumbnail("imageThumbnail")
+                        .imageThumbnail("test1")
                         .startPosition(startPosition)
+                        .betweenPositions(new ArrayList<>())
+                        .endPosition(endPosition)
+                        .routes(null)
+                        .photos(new ArrayList<>())
                         .position(new GeoJsonPoint(2.2, 1.1))
+                        .createdBy("user1")
                         .build()
-        ));
-        foundRouteRepository.save(foundRoutes);
+        );
+        when(foundRouteService.getFoundRoutesByAddress("test")).thenReturn(fakeFoundRoutes);
 
         String expectedJSON = """
-                     [
-                         {
-                             "id": "1",
-                             "routeName": "test",
-                             "hashtags": [
-                                 "tree"
-                             ],
-                             "imageThumbnail": "imageThumbnail",
-                             "startPosition": {
-                                 "lat": 2.2,
-                                 "lon": 1.1
-                             },
-                             "position": {
-                                 "x": 2.2,
-                                 "y": 1.1,
-                                 "type": "Point",
-                                 "coordinates": [
-                                     2.2,
-                                     1.1
-                                 ]
-                             }
-                         }
-                     ]
+                            [
+                                    {
+                                        "id": "1",
+                                        "routeName": "test",
+                                        "hashtags": [
+                                            "tree"
+                                        ],
+                                        "imageThumbnail": "test1",
+                                        "startPosition": {
+                                            "lat": 2.2,
+                                            "lon": 1.1
+                                        },
+                                        "betweenPositions":[
+                                        ],
+                                        "endPosition": {
+                                            "lat": 2.3,
+                                            "lon": 1.12
+                                        },
+                                        "routes": null,
+                                        "photos":[
+                                        ],
+                                        "position": {
+                                            "x": 2.2,
+                                            "y": 1.1,
+                                            "type": "Point",
+                                            "coordinates": [
+                                                2.2,
+                                                1.1
+                                            ]
+                                        },
+                                        "createdBy":"user1"
+                                    }
+                                
+                                ]
+
+
                 """;
         // WHEN & THEN
         mockMvc.perform(
-                        get("/api/found-routes/testaddress"))
+                        get("/api/found-routes/test"))
                 .andExpect(status().isOk())
                 .andExpect(content().json(expectedJSON));
 
